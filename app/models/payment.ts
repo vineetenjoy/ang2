4f62a9825cdfe5d0ebe727972628689b5cf33c70
Vineet <vtiwari@gainsight.com>
2017-02-02 16:46:35 +0530

    ADDED Webpack

--- app/models/payment.ts
+++
@@ -1,8 +0,0 @@
-export class Payment {
-  constructor(
-    public id: string,
-    public amount: number,
-    public merchantName: string,
-    public cardNumber: string,
-    public mode: string) { }
-}
